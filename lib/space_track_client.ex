79381344c8d031a1eecc7d1999e80924461a5af3
James Grantham <james.grantham@gmail.com>
2024-04-04 17:06:13 -0700

    remove unused function

--- lib/space_track_client.ex
+++ lib/space_track_client.ex
@@ -10,34 +10,6 @@ defmodule SpaceTrackClient do
     login(identity, password, retries)
   end
 
-  def login_and_pull do
-    url = "https://www.space-track.org/ajaxauth/login"
-
-    headers = [
-      "Content-Type": "application/x-www-form-urlencoded",
-      Accept: "application/json; Charset=utf-8"
-    ]
-
-    body =
-      URI.encode_query(%{
-        "identity" => "james.grantham@gmail.com",
-        "password" => "xuTbaz-7nyxwe-dorfet",
-        "query" =>
-          "https://www.space-track.org/basicspacedata/query/class/tle_latest/ORDINAL/1/NORAD_CAT_ID/25544,36411,26871,27422/predicates/FILE,EPOCH,TLE_LINE1,TLE_LINE2/format/json"
-      })
-
-    case HTTPoison.post(url, body, headers) do
-      {:ok, %HTTPoison.Response{status_code: 200, body: response_body}} ->
-        {:ok, response_body}
-
-      {:ok, %HTTPoison.Response{status_code: code}} ->
-        {:error, "Failed with status code #{code}"}
-
-      {:error, %HTTPoison.Error{reason: reason}} ->
-        {:error, reason}
-    end
-  end
-
   def extract_cookies(headers) do
     headers
     |> Enum.filter(fn {key, _value} -> String.downcase(key) == "set-cookie" end)
